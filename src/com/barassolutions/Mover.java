e74b8393bcf0642883b10344c22fd62375ef4024
Morgane Austreelis <morgane.austreelis@gmail.com>
2020-01-26 00:27:13 +0100

    [Code cleanup] Deleted unused imports

--- src/com/barassolutions/Mover.java
+++ src/com/barassolutions/Mover.java
@@ -8,18 +8,14 @@ import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import com.barassolutions.Main.Values;
 import java.io.IOException;
 import java.nio.file.CopyOption;
-import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileSystemLoopException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 public class Mover {
